298e3f0e4e3eccbc615b469bfd9be1d7c53c9546
Alan Patrick <alan.patrick.fv@gmail.com>
2022-05-21 15:09:49 -0300

    Complete the menu subcomponent development

--- src/signBoard.js
+++ src/signBoard.js
@@ -1,16 +1,45 @@
-import { configStyle } from "./zonks"
+import { configStyle, p } from "./zonks"
 
 
 const boardStyle = {
-    "width": "300px",
-    "height": "300px",
-    "border": "2px solid black",
+    "height": "fit-content",
+    "border": "4px solid black",
+    "border-radius": "35px",
     "background-color": "white",
+    "padding": "10px 20px",
+    "flex": "1 1 auto"
 }
 
-export default function boardComponent () {
-    const container = document.createElement('div')
-    configStyle(container, boardStyle)
+const boardTitleStyle = {
+    "margin-bottom": "10px",
+    "font-family": "Sail",
+}
+
+const boardItemStyle = {
+    "font-size": "20px",
+    "font-weight": "400",
+    "text-align": "justify",
+    "font-family": "Jetbrains Mono",
+}
+
+export default function boardComponent (boardConfig) {
+    const {title, items, localStyling} = boardConfig;
+
+    const container = document.createElement('div');
+    configStyle(container, boardStyle);
+    configStyle(container, localStyling);
+
+    const titleElem = document.createElement("h3");
+    configStyle(titleElem, boardTitleStyle);
+    titleElem.textContent = title;
+    container.appendChild(titleElem);
+
+    for (const item in items) {
+        const elem = document.createElement("p");
+        configStyle(elem, boardItemStyle);
+        elem.textContent = `${item} ------ ${items[item]}`
+        container.appendChild(elem);
+    }
 
     return container
 }
